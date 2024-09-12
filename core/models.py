357f3233e6d09915ca590da94615180c893e75a4
Baibhav Paudel <101887814+incri@users.noreply.github.com>
2024-09-12 15:16:09 +0545

    adding a defult images to all profile images

--- core/models.py
+++ core/models.py
@@ -10,7 +10,7 @@ class User(AbstractUser):
     email = models.EmailField(unique=True)
     hospital = models.ForeignKey(Hospital, on_delete=models.CASCADE, related_name='users', null=True, blank=True)
     is_hospital_admin = models.BooleanField(default=False)
-    profile_image = models.ImageField(upload_to="lab/profile-images")
+    profile_image = models.ImageField(upload_to="lab/profile-images", default="lab/profile-images/1.jpg")
 
     def __str__(self):
         return self.username
